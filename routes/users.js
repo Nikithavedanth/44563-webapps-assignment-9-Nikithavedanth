ef86a195953c3092bf64d0cd4c6126126e905fcc
Nikithavedanth <nikithavedanth@gmail.com>
2022-10-25 22:10:22 -0500

    updated

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var times=0;
+  times=times+1;
+  res.send('user access are:times');
+ 
+
 });
 
 module.exports = router;
